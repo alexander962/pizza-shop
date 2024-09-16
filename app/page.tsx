d5190b5bac45b5b4366c5a49237e5389b8df81d4
alexander962 <sanek12011996@gmail.com>
2024-09-16 10:36:18 +0300

    add top-bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
-import { Container } from "@/components/shared";
-import { Title } from "@/components/shared/title";
-import { Categories } from "@/components/shared/categories";
+import { TopBar, Container, Title } from "@/components/shared";
 
 export default function Home() {
   return (
     <>
       <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
-
-        <Categories />
       </Container>
+      <TopBar />
     </>
   );
 }
---
+++ components/shared/top-bar.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import { Categories, SortPopup, Container } from "@/components/shared";
+
+interface Props {
+  className?: string;
+}
+
+export const TopBar: FC<Props> = ({ className }) => {
+  return (
+    <div className="sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10">
+      <Container className="flex items-center justify-between">
+        <Categories />
+        <SortPopup />
+      </Container>
+    </div>
+  );
+};
