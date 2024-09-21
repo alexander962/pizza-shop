ab95bc2ea3dfce50f76f619b55ac261d274554b3
alexander962 <sanek12011996@gmail.com>
2024-09-21 12:13:36 +0300

    add search filters

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { FilterCheckbox, RangeSlider, Title } from "@/components/shared";
 import { Input } from "@/components/ui";
+import { CheckboxFiltersGroup } from "@/components/shared/checkbox-filters-group";
 
 interface Props {
   className?: string;
@@ -31,6 +32,88 @@ export const Filters: FC<Props> = ({ className }) => {
 
         <RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
       </div>
+
+      <CheckboxFiltersGroup
+        title="Ингредиенты"
+        className="mt-5"
+        limit={6}
+        defaultItems={[
+          {
+            text: "Сырный соус",
+            value: "1",
+          },
+          {
+            text: "Моццарелла",
+            value: "2",
+          },
+          {
+            text: "Чеснок",
+            value: "3",
+          },
+          {
+            text: "Солённые огурчики",
+            value: "4",
+          },
+          {
+            text: "Красный лук",
+            value: "5",
+          },
+          {
+            text: "Томаты",
+            value: "6",
+          },
+        ]}
+        items={[
+          {
+            text: "Сырный соус",
+            value: "1",
+          },
+          {
+            text: "Моццарелла",
+            value: "2",
+          },
+          {
+            text: "Чеснок",
+            value: "3",
+          },
+          {
+            text: "Солённые огурчики",
+            value: "4",
+          },
+          {
+            text: "Красный лук",
+            value: "5",
+          },
+          {
+            text: "Томаты",
+            value: "6",
+          },
+          {
+            text: "Сырный соус",
+            value: "1",
+          },
+          {
+            text: "Моццарелла",
+            value: "2",
+          },
+          {
+            text: "Чеснок",
+            value: "3",
+          },
+          {
+            text: "Солённые огурчики",
+            value: "4",
+          },
+          {
+            text: "Красный лук",
+            value: "5",
+          },
+          {
+            text: "Томаты",
+            value: "6",
+          },
+        ]}
+      />
     </div>
   );
 };
